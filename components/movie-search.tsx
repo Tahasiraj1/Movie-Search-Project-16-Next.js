327540c899bfa259f1e44c3a55c080dd8172bf99
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-09-27 11:29:56 +0500

    Update movie-search.tsx

--- components/movie-search.tsx
+++ components/movie-search.tsx
@@ -42,7 +42,7 @@ export default function MovieSearch() {
                 throw new Error(data.Error);
             }
             setMovieDetails(data);
-        } catch (error: any) {
+        } catch (error) {
             setError(error.message);
         } finally {
             setLoading(false);
